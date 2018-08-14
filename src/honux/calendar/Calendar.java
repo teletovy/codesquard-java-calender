b456360d6706f1ea0a4743673993a1f65cf5d91a
jung <jung@jung-PC>
2018-08-14 20:40:22 +0900

    project calendar set repeat input time

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,22 +3,29 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public int maxDaysOfMonth(int month) {
 		return MAX_DAYS[month-1];
 	}
-		
+	
 	public static void main(String[] args) {
-		System.out.println("달을 입력하세요.");
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		int month = scan.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month , cal.maxDaysOfMonth(month) );
+		System.out.println("반복횟수를 입력하시오.");
+		int repeat = scan.nextInt();
+		
+		for(int i =1; i<=repeat; i++) {
+			System.out.println("달을 입력하시오.");
+			int month = scan.nextInt();
+			System.out.printf("%d월은 %d일입니다.\n",month,cal.maxDaysOfMonth(month));
+			}
 		scan.close();
-		}}
+		System.out.println("안녕~");
+	}
+	
+}
 		
 
 
