cebfc2c9dce3d537b47d331dcff1dc8ba91529b2
jung <jung@jung-PC>
2018-08-14 18:53:33 +0900

    answer 2 find max days of month

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -4,25 +4,21 @@ import java.util.Scanner;
 
 public class Calendar {
 	
+	private static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+		
 	public static void main(String[] args) {
 		System.out.println("달을 입력하세요.");
 		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int[] maxdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		int result = maxdays[a-1];
-		if(a<1) {
-			System.out.println("오류");
-			
-		}
-		else if (a>12) {
-			System.out.println("오류");
-		}
-		else  {
-			System.out.printf("%d월은 %d일 까지 있습니다",a,result);
-		}
-			
-		}
-		}
+		Calendar cal = new Calendar();
+		int month = scan.nextInt();
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month , cal.maxDaysOfMonth(month) );
+		scan.close();
+		}}
 		
 
 
