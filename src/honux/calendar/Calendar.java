e9d0bb6f03adda8ec75774185b9044510f25bfdc
jung <jung@jung-PC>
2018-08-20 22:27:42 +0900

    project calendar set repeat input times using while

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -10,16 +10,24 @@ public class Calendar {
 	}
 	
 	public static void main(String[] args) {
+		
+		String PROMPT="cal>";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
 		System.out.println("반복횟수를 입력하시오.");
-		int repeat = scan.nextInt();
+		int month = 1;
 		
-		for(int i =1; i<=repeat; i++) {
+		while(true) {
 			System.out.println("달을 입력하시오.");
-			int month = scan.nextInt();
-			System.out.printf("%d월은 %d일입니다.\n",month,cal.maxDaysOfMonth(month));
+			System.out.print(PROMPT);
+			month = scan.nextInt();
+			
+			if(month == -1) {
+				break;
+			} else if (month > 12) {
+				continue;
+			} else System.out.printf("%d월은 %d일입니다.\n",month,cal.maxDaysOfMonth(month));
 			}
 		scan.close();
 		System.out.println("안녕~");
